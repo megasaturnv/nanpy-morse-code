b990645193f15b2c81616c8efcc11a05c0bb3717
megasaturnv <megasaturnv@users.noreply.github.com>
2018-02-07 10:43:37 +0000

    Added debug options

--- nanpyMorseCode.py
+++ nanpyMorseCode.py
@@ -7,8 +7,10 @@ except:
     pass #readline not available
 
 DEBUG_MODE=True
+FAKE_AN_ARDUINO_MODE=FALSE
 MORSE_PIN=13
-unitTime=0.1 #Time of a unit, in seconds
+ARDUINO_SERIAL_DEVICE='/dev/serial0'
+UNIT_TIME=0.1 #Time of a unit, in seconds
 #If the duration of a dot is taken to be one unit then that of a dash is three units. The space between the components of one character is one unit, between characters is three units and between words seven units.
 #dot = 1
 #dash = 3
@@ -44,55 +46,68 @@ def textToMorse(text):
 	return morse
 
 def sendDot():
+	if DEBUG_MODE:
+		print('.', end='')
 	aa.digitalWrite(MORSE_PIN, aa.HIGH)
-	time.sleep(unitTime)
+	time.sleep(UNIT_TIME)
 	aa.digitalWrite(MORSE_PIN, aa.LOW)
-	time.sleep(unitTime)
+	time.sleep(UNIT_TIME)
 
 def sendDash():
+	if DEBUG_MODE:
+		print('-', end='')
 	aa.digitalWrite(MORSE_PIN, aa.HIGH)
-	time.sleep(3*unitTime)
+	time.sleep(3 * UNIT_TIME)
 	aa.digitalWrite(MORSE_PIN, aa.LOW)
-	time.sleep(unitTime)
-
-def sendWordSpace():
-	aa.digitalWrite(MORSE_PIN, aa.LOW)
-	time.sleep(6*unitTime) # 7-1 * unitTime because 1 unit time after every . or -
+	time.sleep(UNIT_TIME)
 
 def sendLetterSpace():
+	if DEBUG_MODE:
+		print(' ', end='')
 	aa.digitalWrite(MORSE_PIN, aa.LOW)
-	time.sleep(2*unitTime) # 3-1 * unitTime  because 1 unit time after every . or -
+	time.sleep(2 * UNIT_TIME) # 3-1 * UNIT_TIME because 1 unit time after every . or -
+
+def sendWordSpace():
+	if DEBUG_MODE:
+		print('/', end='')
+	aa.digitalWrite(MORSE_PIN, aa.LOW)
+	time.sleep(6 * UNIT_TIME) # 7-1 * UNIT_TIME because 1 unit time after every . or -
 
 def sendMorse(morse):
-	for m in morse:
-		if m == ".":
-			sendDot()
-		elif m == "-":
-			sendDash()
-		elif m == "/":
-			sendWordSpace()
-		elif m == " ":
-			sendLetterSpace()
+	if not FAKE_AN_ARDUINO_MODE:
+		for m in morse:
+			if m == ".":
+				sendDot()
+			elif m == "-":
+				sendDash()
+			elif m == "/":
+				sendWordSpace()
+			elif m == " ":
+				sendLetterSpace()
 
-def sendText(text):
-	sendMorse(textToMorse(text))
+if not FAKE_AN_ARDUINO_MODE:
+	connection = nanpy.SerialManager(device=ARDUINO_SERIAL_DEVICE)
 
-connection = nanpy.SerialManager(device='/dev/serial0')
+	aa = nanpy.ArduinoApi(connection=connection)
+	at = nanpy.arduinotree.ArduinoTree(connection=connection)
 
-aa = nanpy.ArduinoApi(connection=connection)
-at = nanpy.arduinotree.ArduinoTree(connection=connection)
-
-aa.pinMode(MORSE_PIN, aa.OUTPUT)
-aa.digitalWrite(MORSE_PIN, aa.LOW)
+	aa.pinMode(MORSE_PIN, aa.OUTPUT)
+	aa.digitalWrite(MORSE_PIN, aa.LOW)
 
 print("Type 'QUIT' to exit the program")
 print("Enter the text you want to send via morse code:")
+
+if DEBUG_MODE:
+	print("Info: Program in DEBUG_MODE")
+if	FAKE_AN_ARDUINO_MODE:
+	print("Info: Program in FAKE_AN_ARDUINO_MODE")
 
 inp = ""
 while inp != "QUIT":
 	inp = input("> ")
 	if inp != "QUIT":
-		sendText(inp)
+		sendMorse(textToMorse(inp))
 
-aa.digitalWrite(MORSE_PIN, aa.LOW)
-aa.pinMode(MORSE_PIN, aa.INPUT)
+if not FAKE_AN_ARDUINO_MODE:
+	aa.digitalWrite(MORSE_PIN, aa.LOW)
+	aa.pinMode(MORSE_PIN, aa.INPUT)
