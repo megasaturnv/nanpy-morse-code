a4eef4de36b686aec7a247cb2f6632fb52d50bb2
megasaturnv <megasaturnv@users.noreply.github.com>
2018-02-23 21:29:48 +0000

    Added ability to invert output

--- nanpyMorseCode.py
+++ nanpyMorseCode.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-import time, nanpy, nanpy.arduinotree
-
-try:
-    import readline
-except:
-    pass #readline not available
-
 DEBUG_MODE=True
-FAKE_AN_ARDUINO_MODE=False
+FAKE_AN_ARDUINO_MODE=True
+INVERT_OUTPUT=False
 MORSE_PIN=13
 ARDUINO_SERIAL_DEVICE='/dev/serial0'
 UNIT_TIME=0.1 #Time of a unit, in seconds
@@ -30,6 +24,23 @@ toMorse = dict({
 "5": ".....", "6": "-....",  "7": "--...",  "8": "---..",  "9": "----.",
 })
 
+import time
+if not FAKE_AN_ARDUINO_MODE:
+	import nanpy, nanpy.arduinotree
+
+try:
+    import readline
+except:
+    pass #readline not available
+
+def digitalWrite(pin, state):
+	if INVERT_OUTPUT:
+		if DEBUG_MODE:
+			print('pinstate: ' + str(state))
+		aa.digitalWrite(MORSE_PIN, state)
+	else:
+		aa.digitalWrite(MORSE_PIN, state)
+
 def textToMorse(text):
 	morse = ""
 	for character in text:
@@ -48,29 +59,29 @@ def textToMorse(text):
 def sendDot():
 	if DEBUG_MODE:
 		print('.', end='', flush=True)
-	aa.digitalWrite(MORSE_PIN, aa.HIGH)
+	digitalWrite(MORSE_PIN, aa.HIGH)
 	time.sleep(UNIT_TIME)
-	aa.digitalWrite(MORSE_PIN, aa.LOW)
+	digitalWrite(MORSE_PIN, aa.LOW)
 	time.sleep(UNIT_TIME)
 
 def sendDash():
 	if DEBUG_MODE:
 		print('-', end='', flush=True)
-	aa.digitalWrite(MORSE_PIN, aa.HIGH)
+	digitalWrite(MORSE_PIN, aa.HIGH)
 	time.sleep(3 * UNIT_TIME)
-	aa.digitalWrite(MORSE_PIN, aa.LOW)
+	digitalWrite(MORSE_PIN, aa.LOW)
 	time.sleep(UNIT_TIME)
 
 def sendLetterSpace():
 	if DEBUG_MODE:
 		print(' ', end='', flush=True)
-	aa.digitalWrite(MORSE_PIN, aa.LOW)
+	digitalWrite(MORSE_PIN, aa.LOW)
 	time.sleep(2 * UNIT_TIME) # 3-1 * UNIT_TIME because 1 unit time after every . or -
 
 def sendWordSpace():
 	if DEBUG_MODE:
 		print('/', end='', flush=True)
-	aa.digitalWrite(MORSE_PIN, aa.LOW)
+	digitalWrite(MORSE_PIN, aa.LOW)
 	time.sleep(6 * UNIT_TIME) # 7-1 * UNIT_TIME because 1 unit time after every . or -
 
 def sendMorse(morse):
@@ -91,16 +102,21 @@ if not FAKE_AN_ARDUINO_MODE:
 	aa = nanpy.ArduinoApi(connection=connection)
 	at = nanpy.arduinotree.ArduinoTree(connection=connection)
 
-	aa.pinMode(MORSE_PIN, aa.OUTPUT)
-	aa.digitalWrite(MORSE_PIN, aa.LOW)
+	#aa.pinMode(MORSE_PIN, aa.OUTPUT)
+	if not INVERT_OUTPUT:
+		aa.digitalWrite(MORSE_PIN, aa.LOW)
+	else:
+		aa.digitalWrite(MORSE_PIN, aa.HIGH)
 
 print("Type 'QUIT' to exit the program")
 print("Enter the text you want to send via morse code:")
 
 if DEBUG_MODE:
-	print("Info: Program in DEBUG_MODE")
-if	FAKE_AN_ARDUINO_MODE:
-	print("Info: Program in FAKE_AN_ARDUINO_MODE")
+	print("Info: Program in DEBUG_MODE. Program will be verbose with extra information.")
+	if FAKE_AN_ARDUINO_MODE:
+		print("Info: Program in FAKE_AN_ARDUINO_MODE. Nanpy commands will not be used.")
+	if INVERT_OUTPUT:
+		print("Info: INVERT_OUTPUT is true. Output will be inverted. Lights are on when output is low.")
 
 inp = ""
 while inp != "QUIT":
@@ -110,6 +126,6 @@ while inp != "QUIT":
 	if inp != "QUIT":
 		sendMorse(textToMorse(inp))
 
-if not FAKE_AN_ARDUINO_MODE:
+if not FAKE_AN_ARDUINO_MODE: #Reset pin so defaults (safe state)
 	aa.digitalWrite(MORSE_PIN, aa.LOW)
 	aa.pinMode(MORSE_PIN, aa.INPUT)
